88b2321ddf169ff144a63f5da973c9ebdd9e178c
Gronier Thibaut <thibaut_gronier@ens.univ-artois.fr>
2021-12-15 17:14:22 +0100

    ticket 4 vraiment terminé

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -3,10 +3,10 @@
     @foreach($series as $s)
         <li><a  href="../series/{{$s->id}}"><img src="{{$s->urlImage}}"/></a>
                 <div>
-                    <p>{{$s->nom}}</p>
-                    <p>{{$s->genre}}</p>
-                    <p>{{$s->langue}}</p>
-                    <p>{{count($s->episodes->unique('saison'))}}</p>
+                    <p><h3>Nom:{{$s->nom}}</h3></p>
+                    <p><h3>Genre:{{$s->genre}}</h3></p>
+                    <p><h3>Langue:{{$s->langue}}</h3></p>
+                    <p><h3>Nombre de saisons:{{count($s->episodes->unique('saison'))}}</h3></p>
                 </div>
 
         </li>
