6b61c0ba95fa4ae348b9408fef5cbc77f91ab7a1
Gronier Thibaut <thibaut_gronier@ens.univ-artois.fr>
2021-12-16 17:44:48 +0100

    fix

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="404css.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<img src="../404.png" alt="404">
+<a href="{{ url('/') }}">Retour à l'accueil</a>
+
+</body>
+</html>
