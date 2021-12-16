ceed902296c389dc09d1a295b7ec5bfe3864aed8
Gronier Thibaut <thibaut_gronier@ens.univ-artois.fr>
2021-12-16 17:50:28 +0100

    version final

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="utf-8">
   <title>Titre de la page</title>
-  <link rel="stylesheet" href="404css.css">
+  <link rel="stylesheet" href="./../css/style.css">
   <script src="script.js"></script>
 </head>
 <body>
     <div class="img_error">
     <img src="{{ asset('img/404.png') }}" alt="404">
 </div>
-    <a href="{{ url('/') }}">Retour à l'accueil</a>
+    <a class="button-36" href="{{ url('/') }}">Retour à l'accueil</a>
 
 </body>
 </html>
