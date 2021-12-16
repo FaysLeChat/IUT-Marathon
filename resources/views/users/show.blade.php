67a36c11fcc3d12621e9e438a577b3963c884e70
FaysLeChat <fayslechat@gmail.com>
2021-12-16 09:59:00 +0100

    Utilisation func seen

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,9 +12,12 @@
         <b>{{ $user->name }}</b>
         <hr>
         <h2>Series récentes</h2>
-        @foreach($seen->take(5)->get() as $s)
-            <a href="/series/{{ \App\Models\Episode::select('*')->from('episodes')->where('id', $s->episode_id)->get()[0]->serie_id }}"><img src="/{{ \App\Models\Episode::select('*')->from('series')->where('id', \App\Models\Episode::select('*')->from('episodes')->where('id', $s->episode_id)->get()[0]->serie_id)->get()[0]->urlImage }}" /></a>
+        @foreach(\App\Http\Controllers\ControllerUsers::userSeries($user->id) as $c)
+            <a href="/series/{{ $c }}"><img src="/{{ \App\Models\Serie::find($c)->urlImage }}" /></a>
         @endforeach
+       {{-- @foreach($seen->take(5)->get() as $s)
+            <a href="/series/{{ \App\Models\Episode::select('*')->from('episodes')->where('id', $s->episode_id)->get()[0]->serie_id }}"><img src="/{{ \App\Models\Episode::select('*')->from('series')->where('id', \App\Models\Episode::select('*')->from('episodes')->where('id', $s->episode_id)->get()[0]->serie_id)->get()[0]->urlImage }}" /></a>
+        @endforeach--}}
         <hr>
         <h2>Episodes récents</h2>
         @foreach($seen->take(5)->get() as $s)
